9ec4c69eca02cec558c4979ec49205cad41f9bad
PyFan <darru2002@gmail.com>
2024-04-18 22:22:25 +0200

    changed acknowledge

--- utils/acknowledge.py
+++ utils/acknowledge.py
@@ -6,7 +6,5 @@ def show_creator_acknowledgement():
     st.markdown(
         "🛠️ Frontend & Backend creado por Daniel Ruiz [Github](https://github.com/paydos) | [LinkedIn](https://www.linkedin.com/in/daniel-ruiz-blanco/)"
     )
-    st.markdown(
-        "👩‍⚖️ Equipo Legal: Saira De Brito | Lucía Ríos | Rodrigo Hurtado | Javier de Rojas"
-    )
+    st.markdown("👩‍⚖️ Equipo Legal: Lucía Ríos | Javier de Rojas")
     st.markdown("Todos los derechos reservados © 2024")
